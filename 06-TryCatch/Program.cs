f2a9f1b945c4d9873d243b599aabc509a2decf16
akinali94 <aliakn.20@gmail.com>
2023-02-14 23:13:37 +0300

    06-TryCatch

---
+++ 06-TryCatch/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _06_TryCatch;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Exception Handling
+        //Hatalar oluşma zamanına göre ikiye ayrılır:
+        //Compile Time Errors: Kodu yazarken derleyicinin bize kızmasıdır.
+        //RunTime Errors: Çalıştırdıktan sonraki hatalardır, bunlarla başa çıkmamız gerekmektedir (handle).
+    }
+}
