b655e3123ec9db8eb57310ba815197f60aa506c0
akinali94 <aliakn.20@gmail.com>
2023-02-16 23:46:09 +0300

    07-IfElse

---
+++ 07-IfElse/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _07_IfElse;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Saate göre bir selamlama programı yazalım.
+        //DateTime kütüphanesinden yararlanalım
+
+        int time = DateTime.Now.Hour;
+
+        if(time >=6 && time < 11)
+            Console.WriteLine("Günaydın!");
+        else if(time <= 18)
+            Console.WriteLine("İyi günler");
+        else
+            Console.WriteLine("İyi Geceler!");
+
+        //Ternary IF
+        
+        string sonuc = time<=18 ? "İyi Günler" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+        /*Soru işareti (?) ise anlamına gelmektedir, eğer ? öncesindeki ifade doğruysa
+        * ? sonrasındaki ifade çalışır. Değilse, : , sonrasındaki ifade çalışmaktadır.
+        */
+
+        string sonuc2 = time>=6 && time < 11 ? "Günaydın" : time<=18 ? "İyi Günler" : "İyi Geceler";
+        Console.WriteLine(sonuc);
+
+    }
+}
